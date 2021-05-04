cf9402a08ec5a7852adc99171df4a9cdbc21b710
alaa-yafi <alaa.yafi25@outlook.com>
2021-05-04 17:48:49 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// ‫ إلغاء الحركة في شاشات الجوال وتهيئة مكتبة wow للعمل
+// تلغى التأثيرات فقط فى النسخة اقل من 4 فى مقاس الجوال
+var wow = new WOW({
+    mobile: false
+});
+wow.init();
+// emailForm على النموذج validate تطبيق التابع
+$(function () {
+    $("#emailform").validate();
+ });
